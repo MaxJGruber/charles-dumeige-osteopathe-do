2a63b38632204f861732fce7836cdb5530fa994b
MaxJGruber <maxjgruber@gmail.com>
2022-06-06 17:33:15 +0200

    Add basic SEO

---
+++ config.js
@@ -0,0 +1,20 @@
+// The page title
+export const TITLE = "Charles Dumeige: ostéopathe D.O";
+
+// The description shown in Google Search results and on social media networks
+// Should be max ~150 characters.
+export const META_DESCRIPTION =
+  "Charles Dumeige, est spécialisé en ostéopathie générale et cranienne, viscérale. Il prend en charge ses patients au cabinet ou à domicile, même le dimanche. Il prend en charge les nourissons et bébés, les enfants et adolescents, les adultes (hommes, femme et femmes enceintes), les séniors et les sportifs.";
+
+// The URL of the deployed page (used in meta tags for SMO)
+// Example: https://www.domain.com/
+export const URL = null;
+
+// The preview image shown in social networks and when sharing on messengers,
+// like WhatsApp and others
+// The URL should be absolute, e.g.: https//www.domain.com/static/preview.jpg
+// Recommended size: 1200 x 627
+export const META_IMAGE = null;
+
+// The Google Analytics tracking ID
+export const GA_TRACKING_ID = "";
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import NextNProgress from "nextjs-progressbar";
+import { TITLE, META_DESCRIPTION, URL, META_IMAGE } from "root/config";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+        title={TITLE}
+        description={META_DESCRIPTION}
+        openGraph={{ url: URL, images: [{ url: META_IMAGE }] }}
+      />
       <NextNProgress />
       <Component {...pageProps} />
     </>
