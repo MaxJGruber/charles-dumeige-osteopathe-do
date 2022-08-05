15ff4af536db327e7ef9c0f77f3def33617ddd18
MaxJGruber <maxjgruber@gmail.com>
2022-08-05 18:40:13 +0200

    Add components to landing page

---
+++ components/Featured.js
@@ -0,0 +1,78 @@
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+} from "@heroicons/react/outline";
+
+const features = [
+  {
+    id: 1,
+    name: "A l'écoute du patient",
+    description:
+      "Une attitude active dans l'approche relationnelle avec le patient, toujours à l'écoute et disponible.",
+    icon: GlobeAltIcon,
+  },
+  {
+    id: 2,
+    name: '"Un mal pour un bien"',
+    description:
+      "Suivant l'algie du patient, il se peut qu'il ressente des douleurs durant la séance mais qui finissent par aboutir à un sentiment de bien-être à la fin de la consultation.",
+    icon: ScaleIcon,
+  },
+  {
+    id: 3,
+    name: "Résultats rapides",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: LightningBoltIcon,
+  },
+  {
+    id: 4,
+    name: "Notifications mobiles",
+    description:
+      "Disponible par téléphone & email pour les contacts directs. Prise de rendez-vous possible sur doctolib.fr.",
+    icon: AnnotationIcon,
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="py-12 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-lightblue font-semibold tracking-wide uppercase">
+            L'OSTÉOPATHIE
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Générale - Viscérale - Crânienne
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Dans son cabinet ou à domicile, Charles Dumeige prend en charge tout
+            type de patient.
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-lightblue text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                    {feature.name}
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import HeroSection from "components/HeroSection";
+import Featured from "components/Featured";
+import Hook from "components/Hook";
+import Contact from "components/Contact";
+import PriceList from "components/PriceList";
+import Reimbursements from "components/Reimbursements";
+import PracticalInfo from "components/PracticalInfo";
 
 const Index = () => {
   return (
     <>
       <HeroSection />
+      <Featured />
+      <Hook />
+      <Contact />
+      <PriceList />
+      <Reimbursements />
+      <PracticalInfo />
     </>
   );
 };
