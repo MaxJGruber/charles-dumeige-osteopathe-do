0e70b443a97c1db3b3bb619566ea487b50c2106c
MaxJGruber <maxjgruber@gmail.com>
2022-08-12 17:03:26 +0200

    Remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    // fontFamily: {
-    //   display: ["Oswald"],
-    //   body: ["Avenir"],
-    // },
     extend: {
       colors: {
         darkblue: "#21556d",
