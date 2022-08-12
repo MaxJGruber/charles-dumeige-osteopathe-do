20290abc283ac4ddb05eac3e35dd142bc8f971a2
MaxJGruber <maxjgruber@gmail.com>
2022-08-12 16:06:10 +0200

    Add next links to all links in home page

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -31,16 +31,15 @@ const HeroSection = () => (
             >
               <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                 <div className="flex items-center justify-between h-full w-full md:w-auto">
-                  <a href="/">
-                    <span className="sr-only">Logo</span>
+                  <Link href="/" passhref>
                     <img
                       alt="Logo"
                       className="h-8 w-auto sm:h-28"
                       src="static/logo.jpg"
                     />
-                  </a>
+                  </Link>
                   <div className="-mr-2 flex items-center md:hidden">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightblue">
                       <span className="sr-only">Open main menu</span>
                       <MenuIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
@@ -76,15 +75,15 @@ const HeroSection = () => (
             >
               <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                 <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
+                  <Link href="/" passhref>
                     <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                      alt=""
+                      alt="Logo"
+                      className="h-8 w-auto sm:h-28"
+                      src="static/logo.jpg"
                     />
-                  </div>
+                  </Link>
                   <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightblue">
                       <span className="sr-only">Close main menu</span>
                       <XIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
@@ -92,13 +91,14 @@ const HeroSection = () => (
                 </div>
                 <div className="px-2 pt-2 pb-3 space-y-1">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
+                      passHref
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
