dd195b3e7aad38416d8f77d6b617237f7e803e09
MaxJGruber <maxjgruber@gmail.com>
2022-08-16 10:12:37 +0200

    Rename components

--- components/Hook.js
+++ components/Hook.js
@@ -1,31 +1,31 @@
-export default function Example() {
-  return (
-    <div className="bg-gray-50">
-      <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Assez entendu?</span>
-          <span className="block text-lightblue">Prenez rendez-vous.</span>
-        </h2>
-        <div className="mt-8 lg:mt-0 lg:flex-shrink-0">
-          <div className="inline-flex rounded-md shadow">
-            <a
-              href="https://www.doctolib.fr/osteopathe/meaux/charles-dumeige"
-              target="_blank"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-lightblue hover:bg-darkblue"
-            >
-              Prenez RDV sur doctolib.fr
-            </a>
-          </div>
-          <div className="ml-3 inline-flex rounded-md shadow">
-            <a
-              href="tel:0638697716"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-lightblue hover:bg-lightblueHover hover:text-darkblue bg-white hook-btn2"
-            >
-              Appelez le 06 38 69 77 16
-            </a>
-          </div>
+const Hook = () => (
+  <div className="bg-gray-50">
+    <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:flex lg:items-center lg:justify-between">
+      <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <span className="block">Assez entendu?</span>
+        <span className="block text-lightblue">Prenez rendez-vous.</span>
+      </h2>
+      <div className="mt-8 lg:mt-0 lg:flex-shrink-0">
+        <div className="inline-flex rounded-md shadow">
+          <a
+            href="https://www.doctolib.fr/osteopathe/meaux/charles-dumeige"
+            target="_blank"
+            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-lightblue hover:bg-darkblue"
+          >
+            Prenez RDV sur doctolib.fr
+          </a>
+        </div>
+        <div className="ml-3 inline-flex rounded-md shadow">
+          <a
+            href="tel:0638697716"
+            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-lightblue hover:bg-lightblueHover hover:text-darkblue bg-white hook-btn2"
+          >
+            Appelez le 06 38 69 77 16
+          </a>
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
+
+export default Hook;
--- components/PracticalInfo.js
+++ components/PracticalInfo.js
@@ -42,56 +42,56 @@ const features = [
   },
 ];
 
-export default function Example() {
-  return (
-    <div className="py-12 bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2 className="text-base text-lightblue font-semibold tracking-wide uppercase">
-            INFORMATIONS PRATIQUES
-          </h2>
-        </div>
-        <div className="mt-10">
-          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt className="flex items-center">
-                  <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md ${
-                      feature.isWarning ? "bg-yellow-500" : "bg-lightblue"
-                    } text-white`}
-                  >
-                    <FontAwesomeIcon icon={feature.icon} size="2x" />
-                  </div>
-                  <p
-                    className={`ml-16 text-lg leading-6 font-medium ${
-                      feature.isWarning ? "text-yellow-500" : "text-gray-900"
-                    }`}
-                  >
-                    {feature.name}
-                    {feature.isCreditCard && (
-                      <>
-                        {" "}
-                        (
-                        <span className="mx-1">
-                          <FontAwesomeIcon icon={faCcMastercard} size="lg" />
-                        </span>
-                        <span className="mx-1">
-                          <FontAwesomeIcon icon={faCcVisa} size="lg" />
-                        </span>
-                        <span className="mx-1">
-                          <FontAwesomeIcon icon={faCcAmex} size="lg" />
-                        </span>
-                        )
-                      </>
-                    )}
-                  </p>
-                </dt>
-              </div>
-            ))}
-          </dl>
-        </div>
+const PracticalInfo = () => (
+  <div className="py-12 bg-white">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="lg:text-center">
+        <h2 className="text-base text-lightblue font-semibold tracking-wide uppercase">
+          INFORMATIONS PRATIQUES
+        </h2>
+      </div>
+      <div className="mt-10">
+        <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative">
+              <dt className="flex items-center">
+                <div
+                  className={`absolute flex items-center justify-center h-12 w-12 rounded-md ${
+                    feature.isWarning ? "bg-yellow-500" : "bg-lightblue"
+                  } text-white`}
+                >
+                  <FontAwesomeIcon icon={feature.icon} size="2x" />
+                </div>
+                <p
+                  className={`ml-16 text-lg leading-6 font-medium ${
+                    feature.isWarning ? "text-yellow-500" : "text-gray-900"
+                  }`}
+                >
+                  {feature.name}
+                  {feature.isCreditCard && (
+                    <>
+                      {" "}
+                      (
+                      <span className="mx-1">
+                        <FontAwesomeIcon icon={faCcMastercard} size="lg" />
+                      </span>
+                      <span className="mx-1">
+                        <FontAwesomeIcon icon={faCcVisa} size="lg" />
+                      </span>
+                      <span className="mx-1">
+                        <FontAwesomeIcon icon={faCcAmex} size="lg" />
+                      </span>
+                      )
+                    </>
+                  )}
+                </p>
+              </dt>
+            </div>
+          ))}
+        </dl>
       </div>
     </div>
-  );
-}
+  </div>
+);
+
+export default PracticalInfo;
