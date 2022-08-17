737064befd9af62b2035f98c6d605976d4ed0d12
MaxJGruber <maxjgruber@gmail.com>
2022-08-17 15:50:54 +0200

    Add content to legal section

--- pages/legal.js
+++ pages/legal.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   CameraIcon,
   BookOpenIcon,
@@ -35,13 +36,11 @@ const supportLinks = [
 
 const Legal = () => (
   <div className="bg-white">
-    {/* Header */}
     <div className="relative pb-32 bg-gray-800">
       <div className="absolute inset-0">
         <img
           className="w-full h-full object-cover"
-          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
-          alt=""
+          src="static/legal-pic.jpeg"
         />
         <div className="absolute inset-0 bg-darkblue mix-blend-multiply" />
       </div>
@@ -50,10 +49,19 @@ const Legal = () => (
           Mentions Légales
         </h1>
         <p className="mt-6 max-w-3xl text-xl text-gray-300">
-          Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id
-          malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.
-          Dui, amet, nec sit pulvinar.
+          Le site a été dévelopé par Maximilian Gruber.
         </p>
+        <p className="mt-6 max-w-3xl text-xl text-gray-300">
+          Ce site est hébérgé par nos soins avec le nom de domaine acheté chez
+          GoDaddy.
+        </p>
+        <div className="mt-6 ">
+          <Link href="/">
+            <a className="text-xl text-gray-300 hover:text-lightblue">
+              Retour à la page d'acceuil<span> &rarr;</span>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
 
@@ -70,11 +78,13 @@ const Legal = () => (
           >
             <div className="flex-1 relative pt-16 px-6 pb-8 md:px-8">
               <div className="absolute top-0 p-5 inline-block bg-lightblue rounded-xl shadow-lg transform -translate-y-1/2">
-                <link.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                <link.icon className="h-6 w-6 text-white" />
               </div>
               <h3 className="text-xl font-medium text-gray-900">{link.name}</h3>
-              {link.description.map((line) => (
-                <p className="mt-4 text-base text-gray-500">{line}</p>
+              {link.description.map((line, i) => (
+                <p key={i} className="mt-4 text-base text-gray-500">
+                  {line}
+                </p>
               ))}
             </div>
           </div>
