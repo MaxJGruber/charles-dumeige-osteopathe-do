140c62fbe4a3d1392478691cd1189f40528674f5
MaxJGruber <maxjgruber@gmail.com>
2026-09-16 14:27:48 +0200

    Add design tokens and typefaces drawn from the practice logo

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,16 @@ class MyDocument extends Document {
       <Html lang="fr">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600&family=Inter:wght@400;500;600&display=swap"
+          />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <meta name="theme-color" content="#0F6A8F" />
